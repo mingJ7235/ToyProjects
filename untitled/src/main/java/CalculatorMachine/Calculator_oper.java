474d0e168f935dcc2a97cce3861620c030b2df8d
김민재 <joshuara7235@gmail.com>
2021-06-14 21:33:26 +0900

    Calculator machine using only user's equation

--- untitled/src/main/java/CalculatorMachine/Calculator_oper.java
+++ untitled/src/main/java/CalculatorMachine/Calculator_oper.java
@@ -1,30 +1,26 @@
 package CalculatorMachine;
 
+import java.sql.SQLSyntaxErrorException;
 import java.util.Scanner;
 
 public class Calculator_oper {
     public static void main(String[] args) {
         boolean loopCheck = true;
         Scanner sc = new Scanner(System.in);
-        String oper = "";
-        while (loopCheck) {
-            System.out.println("계산기 입니다.\n" +
-                    "1번째 숫자 : ");
-            double num1 = Double.parseDouble(sc.next());
-            System.out.println("연산자 : (+, -, *, /)");
-            oper = sc.next();
-            System.out.println("2번째 숫자 : ");
-            double num2 = Double.parseDouble(sc.next());
+        String data = "";
 
-            System.out.println("연산을 계속 하시겠습니까? (y/n)");
-            String calCheck = sc.next();
+        System.out.println("계산하고 싶은 수식을 자유롭게 써주세요 (ex> 4.5 * 13 + 23");
+        String equation = sc.nextLine();
 
-            if(calCheck.equals("y") || calCheck.equals("Y")) {
-                while (loopCheck) {
+        for (int i = 0 ; i < equation.length(); i ++) {
+            char letter = equation.charAt(i);
+            if (letter == ' ') {
 
-                }
+            } else {
+                data += letter;
             }
         }
+        System.out.println(data);
     }
 
 
