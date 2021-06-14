5bbf7517f8ec1117e1a1170a160a035db4e3bb58
김민재 <joshuara7235@gmail.com>
2021-06-14 21:46:42 +0900

    Logic basic completed for equation calculator

--- untitled/src/main/java/CalculatorMachine/Calculator_oper.java
+++ untitled/src/main/java/CalculatorMachine/Calculator_oper.java
@@ -1,5 +1,6 @@
 package CalculatorMachine;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import java.sql.SQLSyntaxErrorException;
 import java.util.Scanner;
 
@@ -7,20 +8,44 @@ public class Calculator_oper {
     public static void main(String[] args) {
         boolean loopCheck = true;
         Scanner sc = new Scanner(System.in);
-        String data = "";
+        String user_data = "";
 
         System.out.println("계산하고 싶은 수식을 자유롭게 써주세요 (ex> 4.5 * 13 + 23");
         String equation = sc.nextLine();
 
+        //사용자의 공식 정제 (띄어쓰기 없이 정제하기)
         for (int i = 0 ; i < equation.length(); i ++) {
             char letter = equation.charAt(i);
             if (letter == ' ') {
 
             } else {
-                data += letter;
+                user_data += letter;
             }
         }
-        System.out.println(data);
+
+        System.out.println("요청하신 공식 '" + user_data + "'의 계산을 실시하겠습니다. 헤헤");
+
+        //정제된 공식을 세분화 시킬것
+
+        for (int i = 0; i < user_data.length();  i ++) {
+            char letter = user_data.charAt(i);
+            String compare_number = "1234567890";
+            String compare_oper = "+-*/";
+
+            for (int j = 0; j< compare_number.length(); j ++) {
+                if (letter == compare_number.charAt(j)){
+
+                } else if (letter == compare_oper.charAt(j)) {
+
+                } else if (letter == '.'){
+
+                } else {
+                    System.out.println("'" + user_data +"' 는 잘못된 수식입니다. 프로그램을 종료합니다.");
+                    break;
+                }
+            }
+        }
+
     }
 
 
