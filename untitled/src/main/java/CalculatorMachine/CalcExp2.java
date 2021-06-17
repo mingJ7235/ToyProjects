05ef308cc61936ba6fc833840c5a37a1250d0bf3
김민재 <joshuara7235@gmail.com>
2021-06-17 17:24:35 +0900

    calculator for equation

--- untitled/src/main/java/CalculatorMachine/CalcExp2.java
+++ untitled/src/main/java/CalculatorMachine/CalcExp2.java
@@ -5,10 +5,13 @@ import java.util.*;
 public class CalcExp2 {
     public static void main(String[] args) {
         int cnt = 0, cnt_op = 0, i ;
-        List<Integer> v = new ArrayList<>();
+        List<String> scumNums = new ArrayList<>();
         List<String> op = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
+        String pre_equation = "";
+        String arrNum = "0123456789";
+        String arrOper = "+-*/";
 
         //사용자에게 공백 분리 요청말고 할수있는 방법 찾아보기
         //두번처리해야함. 우선 사용자에게 받은 데이터를 공백없이 하나 가공하고
@@ -21,9 +24,33 @@ public class CalcExp2 {
             if(pre_exp.charAt(i) == ' ') {
 
             } else {
-
+                pre_equation += pre_exp.charAt(i);
             }
 
+            System.out.println("입력하신 수식 '" + pre_equation +"'을 계산합니다.");
+        }
+
+        for (i = 0; i < pre_equation.length(); i ++) {
+            //자른 숫자를 넣을 String변수
+            String scumNum = "";
+            for (int j = 0; j < arrNum.length(); j ++) {
+                //숫자 검사 (반복 돌려서 숫자 배열에 하나라도 일치할 경우)
+                if (pre_equation.charAt(i) == arrNum.charAt(j)) {
+                    if (pre_equation.charAt(i+1) == arrNum.charAt(j)){
+                        scumNum += pre_equation.charAt(i);
+                    }else {
+                        //pre_equation 다음 문자가 숫자가 아닌경우
+                    }
+                    scumNum += " "; //공백 추가 (나중의 구분점)
+                    scumNums.add(scumNum); //문자 하나 넣기
+                }
+            }
+
+            for (int j = 0; j < arrOper.length(); j ++) {
+                if(pre_equation.charAt(i) == arrOper.charAt(j)) {
+                    //oper 인경우
+                }
+            }
         }
 
 
