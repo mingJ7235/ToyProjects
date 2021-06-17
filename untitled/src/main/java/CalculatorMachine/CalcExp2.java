c51ec2c3a7d886639113847138b37fa6937676e1
김민재 <joshuara7235@gmail.com>
2021-06-17 17:05:59 +0900

    Logic for expression division for calculating equation from users

--- untitled/src/main/java/CalculatorMachine/CalcExp2.java
+++ untitled/src/main/java/CalculatorMachine/CalcExp2.java
@@ -1,9 +1,6 @@
 package CalculatorMachine;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
 
 public class CalcExp2 {
     public static void main(String[] args) {
@@ -16,7 +13,22 @@ public class CalcExp2 {
         //사용자에게 공백 분리 요청말고 할수있는 방법 찾아보기
         //두번처리해야함. 우선 사용자에게 받은 데이터를 공백없이 하나 가공하고
         //다시 이 데이터를 " " 을 주어서 나중에 토큰으로 잘라야함. -> 공백을 내가 주는 건데, 이것에 대한 로직이필요함
-        System.out.println("수식을 입력해주세요 (단, 숫자와 연산자는 공백으로 분리해주세요");
+        System.out.println("수식을 입력해주세요 (ex : 1 * 3 - 2");
+
+        String pre_exp = sc.nextLine();
+
+        for (i = 0; i < pre_exp.length(); i ++) {
+            if(pre_exp.charAt(i) == ' ') {
+
+            } else {
+
+            }
+
+        }
+
+
+        StringTokenizer st = new StringTokenizer(pre_exp, " ");
+
 
     }
 
