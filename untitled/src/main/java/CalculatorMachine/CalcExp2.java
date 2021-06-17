f928e268cf75112bf197b9acd77ba2e79bc716cc
김민재 <joshuara7235@gmail.com>
2021-06-17 11:15:21 +0900

    New toy - Calc Exp 2

---
+++ untitled/src/main/java/CalculatorMachine/CalcExp2.java
@@ -0,0 +1,21 @@
+package CalculatorMachine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class CalcExp2 {
+    public static void main(String[] args) {
+        int cnt = 0, cnt_op = 0, i ;
+        List<Integer> v = new ArrayList<>();
+        List<String> op = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        //사용자에게 공백 분리 요청말고 할수있는 방법 찾아보기
+        System.out.println("수식을 입력해주세요 (단, 숫자와 연산자는 공백으로 분리해주세요");
+
+    }
+    
+}
