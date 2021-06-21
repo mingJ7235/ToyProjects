340fd9502f6097e67633de0d409c576c73919260
김민재 <joshuara7235@gmail.com>
2021-06-22 00:00:05 +0900

    Board Controller, Repository, Service

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
@@ -1,4 +1,35 @@
 package com.project.springbootproject.controller;
 
+import com.project.springbootproject.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping ("board/*")
+@AllArgsConstructor
 public class BoardController {
+    private BoardService boardService;
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping ("/post")
+    public String write () {
+        return "board/write.html";
+    }
+
+    @PostMapping ("/post")
+    public String write () {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+
+    }
+
+
 }
--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/BoardService.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/BoardService.java
@@ -1,4 +1,19 @@
 package com.project.springbootproject.service;
 
+import com.project.springbootproject.dto.BoardDto;
+import com.project.springbootproject.repository.BoardRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
 public class BoardService {
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Long savePost (BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
 }
