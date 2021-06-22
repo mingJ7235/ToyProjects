7d9ec78ebf132dd3a3d91ec05bdc22b106f4ade8
김민재 <joshuara7235@gmail.com>
2021-06-22 22:33:42 +0900

    Succeed load Board List from MySQL DB using JPA

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
@@ -4,9 +4,12 @@ import com.project.springbootproject.dto.BoardDto;
 import com.project.springbootproject.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -14,7 +17,13 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) {
+        List<BoardDto> boardList = boardService.getBoardlist();
+
+        //Presentation TIER 에 넘길 변수 boardList
+        //Model 객체를 통해 addAttribute로 Presentation TIER에 데이터를 전달한다.
+        model.addAttribute("boardList", boardList);
+
         return "board/list.html";
     }
 
@@ -30,6 +39,7 @@ public class BoardController {
         return "redirect:/";
 
     }
+
 
 
 }
--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/BoardService.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/BoardService.java
@@ -1,11 +1,14 @@
 package com.project.springbootproject.service;
 
+import com.project.springbootproject.domain.entity.BoardEntity;
 import com.project.springbootproject.dto.BoardDto;
 import com.project.springbootproject.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +18,25 @@ public class BoardService {
     @Transactional
     public Long savePost (BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardlist() {
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDto);
+        }
+
+        return boardDtoList;
     }
 }
