eee359127491fa534dc614794489ef3c4ec7b8a9
김민재 <joshuara7235@gmail.com>
2021-06-23 18:01:57 +0900

    implementation for Security on build.gradle and SecurityConfig.java file build

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/config/SecurityConfig.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/config/SecurityConfig.java
@@ -1,4 +1,60 @@
 package com.project.springbootproject.config;
 
-public class SecurityConfig {
+import com.project.springbootproject.service.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private MemberService memberService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        static 디렉토리의 하위파일 목록은 인증 무시. 즉, 항상 통과하도록 설정하는 것임
+        web.ignoring().antMatchers("/css/**","/js/**","/img/**","/lib/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //페이지 권한 설정
+                        .antMatchers("/admin/**").hasRole("ADMIN") //ENUM 클래스인 Role에서 정의!
+                    .antMatchers("/user/myinfo").hasRole("MEMBER")
+                    .antMatchers("/**").permitAll()
+                .and() //로그인 설정
+                       .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/user/login/result")
+                    .permitAll()
+                .and() //로그아웃 설정
+                        .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                    .logoutSuccessUrl("/user/logout/result")
+                    .invalidateHttpSession(true)
+                .and()
+                    //403예외처리 핸들링
+                        .exceptionHandling().accessDeniedPage("/user/denied");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+    }
 }
