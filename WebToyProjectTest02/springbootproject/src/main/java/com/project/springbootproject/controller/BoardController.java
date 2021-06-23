2107d2be06395b1fc500b27495fba5bdf688297a
김민재 <joshuara7235@gmail.com>
2021-06-23 12:45:26 +0900

    add mapper method on BoardController.java

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/controller/BoardController.java
@@ -2,12 +2,11 @@ package com.project.springbootproject.controller;
 
 import com.project.springbootproject.dto.BoardDto;
 import com.project.springbootproject.service.BoardService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,8 +36,38 @@ public class BoardController {
         boardService.savePost(boardDto);
 
         return "redirect:/";
-
     }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit (@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+        model.addAttribute("boardDto", boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+
 
 
 
