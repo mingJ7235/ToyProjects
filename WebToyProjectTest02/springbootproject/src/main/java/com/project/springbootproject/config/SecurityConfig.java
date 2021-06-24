0c95d03c8dae07a0e66448ec76b97351877396aa
김민재 <joshuara7235@gmail.com>
2021-06-24 14:44:27 +0900

    Presentation TIER completed for member

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/config/SecurityConfig.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/config/SecurityConfig.java
@@ -20,41 +20,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private MemberService memberService;
 
     @Bean
-    public PasswordEncoder passwordEncoder () {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-//        static 디렉토리의 하위파일 목록은 인증 무시. 즉, 항상 통과하도록 설정하는 것임
-        web.ignoring().antMatchers("/css/**","/js/**","/img/**","/lib/**");
+    public void configure(WebSecurity web) throws Exception
+    {
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                //페이지 권한 설정
-                        .antMatchers("/admin/**").hasRole("ADMIN") //ENUM 클래스인 Role 에서 정의!
-                    .antMatchers("/user/myinfo").hasRole("MEMBER")
-                    .antMatchers("/**").permitAll()
-                .and() //로그인 설정
-                       .formLogin()
-                    .loginPage("/user/login")
-                    .defaultSuccessUrl("/user/login/result")
-                    .permitAll()
-                .and() //로그아웃 설정
-                        .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                    .logoutSuccessUrl("/user/logout/result")
-                    .invalidateHttpSession(true)
+                // 페이지 권한 설정
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/myinfo").hasRole("MEMBER")
+                .antMatchers("/**").permitAll()
+                .and() // 로그인 설정
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/user/login/result")
+                .permitAll()
+                .and() // 로그아웃 설정
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/user/logout/result")
+                .invalidateHttpSession(true)
                 .and()
-                    //403예외처리 핸들링
-                        .exceptionHandling().accessDeniedPage("/user/denied");
-
+                // 403 예외처리 핸들링
+                .exceptionHandling().accessDeniedPage("/user/denied");
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 }
