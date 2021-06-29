4d6925e3b8196214f7fc9458e32ed1fd17088c34
김민재 <joshuara7235@gmail.com>
2021-06-29 10:42:02 +0900

    File service method for Convert from Entity to Dto for connection between controller and service

--- WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/FileService.java
+++ WebToyProjectTest02/springbootproject/src/main/java/com/project/springbootproject/service/FileService.java
@@ -24,7 +24,8 @@ public class FileService {
         return galleryDtoList;
 
     }
-
+    //controller < - > service 통신간에 dto 객체를 사용하기 위해 repository로 부터 얻은 entity 객체를 dto로 변환하는 매서드이다.
+    //매개변수로 entity를 받고 (repository를 통해 얻은) return값으로 dto를 반환한다.
     private GalleryDto convertEntityToDto (GalleryEntity galleryEntity) {
 
         return GalleryDto.builder()
