eb7ed27fc91e0c6258b5220cf7107fa6aaeccd43
김민재 <joshuara7235@gmail.com>
2021-07-04 17:39:46 +0900

    OAuth2User service configuration.. complex :(

--- awsSpringTest/src/main/java/com/joshua/springweb/awsspring/config/auth/CustomOAuth2UserService.java
+++ awsSpringTest/src/main/java/com/joshua/springweb/awsspring/config/auth/CustomOAuth2UserService.java
@@ -1,4 +1,60 @@
 package com.joshua.springweb.awsspring.config.auth;
 
-public class CustomOAuth2UserService {
+import com.joshua.springweb.awsspring.domain.user.User;
+import com.joshua.springweb.awsspring.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+//이 클래스에서는 구글 로그인 이후 가져온 사용자의 정보 (email, name, picture 등)들을 기반으로
+//가입 및 정보 수정, 세션 저장등의 기능을 지원한다.
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            //현재 로그인 진행중인 서비스를 구분하는 코드. (구글인지 네이버인지 등을 구분해준다.)
+            //구글만 사용하는 불필요한 값일 수 있으나, 이후 네이버 로그인 연동시에 네이버 로그인 인지, 구글 로그인 인지 구븐하기 위해 사용한다.
+
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                                        .getUserInfoEndpoint().getUserNameAttributeName();
+            //
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()))
+        , attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate (OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
 }
