3e3cb3656bf1bd8ce5c512e22eadb6e00604ae09
김민재 <joshuara7235@gmail.com>
2021-07-04 11:56:19 +0900

    Spring Security configuration

---
+++ awsSpringTest/src/main/java/com/joshua/springweb/awsspring/config/auth/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.joshua.springweb.awsspring.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity //Spring Security 설정들을 활성화 시켜준다. 이것을 사용해야한다. enable!!!
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable() //h2-console 화면을 사용하기위해 해당 옵션들을 disable 해야한다.
+    }
+}
---
+++ awsSpringTest/src/main/java/com/joshua/springweb/awsspring/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.joshua.springweb.awsspring.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    //소설 로그인으로 반환 되는 갑슺 우 email을 통해 이미 생성된 사용자인지, 처음 가입하는 사용자인지 판단하기 위한 메소드
+    Optional<User> findByEmail (String email);
+}
