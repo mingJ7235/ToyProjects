99b946a937db2c8b4f6c5a4e949b391745601370
Minjae <joshuara7235@spiderkim.com>
2021-08-02 11:45:31 +0900

    Specification practice for template search

---
+++ Kakao_test/dev_module/src/main/java/com/kakao/conditions/TemplateSpecs.java
@@ -0,0 +1,23 @@
+package com.kakao.conditions;
+
+import com.kakao.model.KakaoTemplate;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class TemplateSpecs {
+
+    public static Specification<KakaoTemplate> contentLike (final String keyword) {
+        return new Specification<KakaoTemplate>() {
+            @Override
+            public Predicate toPredicate(Root<KakaoTemplate> root,
+                                         CriteriaQuery<?> query,
+                                         CriteriaBuilder cb) {
+                return cb.like(root.get("contents"),"%" + keyword + "%");
+            }
+        };
+    }
+}
--- Kakao_test/dev_module/src/main/java/com/kakao/repository/KakaoTemplateRepository.java
+++ Kakao_test/dev_module/src/main/java/com/kakao/repository/KakaoTemplateRepository.java
@@ -1,7 +1,12 @@
 package com.kakao.repository;
 
 import com.kakao.model.KakaoTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KakaoTemplateRepository extends JpaRepository<KakaoTemplate, Long> {
+
+    Page<KakaoTemplate> finaAll (Specification<KakaoTemplate> spec, Pageable pageable);
 }
