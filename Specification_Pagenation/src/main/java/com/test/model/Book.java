27ed511dbba4f981bc62bd4d66cc85afa1953726
Minjae <joshuara7235@spiderkim.com>
2021-08-05 11:46:44 +0900

    Making book entity and git test

---
+++ Specification_Pagenation/src/main/java/com/test/model/Book.java
@@ -0,0 +1,20 @@
+package com.test.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Book {
+
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    public Long id;
+}
